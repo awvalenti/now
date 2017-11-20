dae541db3c171895231e3924079d2d2dfdd3d1ba
Andre Willik Valenti <awvalenti@gmail.com>
2017-11-20 11:58:12 -0200

    Formatting

--- src/com/github/awvalenti/now/mvc/model/factory/ModelFactory.java
+++ src/com/github/awvalenti/now/mvc/model/factory/ModelFactory.java
@@ -7,10 +7,9 @@ import com.github.awvalenti.now.mvc.model.Observer;
 
 public class ModelFactory {
 
-	public static Model forTcc(Observer observer,
-			String tccDirectory) {
-		return new Model(tccDirectory + "\\tcc -run -", Charset.forName("UTF-8"),
-				observer);
+	public static Model forTcc(Observer observer, String tccDirectory) {
+		return new Model(tccDirectory + "\\tcc -run -",
+				Charset.forName("UTF-8"), observer);
 	}
 
 }
