7cde42996eb74922158b4866a7c4a25b9f90a3ba
Andre Willik Valenti <awvalenti@gmail.com>
2017-11-20 18:25:11 -0200

    Avoiding recompilation when user presses, e.g., CTRL+C

--- src/com/github/awvalenti/now/mvc/controller/Controller.java
+++ src/com/github/awvalenti/now/mvc/controller/Controller.java
@@ -1,13 +1,17 @@
 package com.github.awvalenti.now.mvc.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import com.github.awvalenti.now.mvc.model.Model;
 import com.github.awvalenti.now.mvc.model.Observer;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 
-public class Controller implements Observer {
+public class Controller implements Observer, Initializable {
 
 	@FXML
 	private TextArea txtSourceCode;
@@ -18,15 +22,26 @@ public class Controller implements Observer {
 
 	private Model model;
 
+	private String lastSourceCode;
+
 	public void setModel(Model model) {
 		this.model = model;
 	}
 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		lastSourceCode = txtSourceCode.getText();
+	}
+
 	@FXML
-	private void codeTyped() {
-		Platform.runLater(() -> {
-			model.run(txtSourceCode.getText());
-		});
+	private synchronized void codeTyped() {
+		String currentSourceCode = txtSourceCode.getText();
+
+		if (currentSourceCode.equals(lastSourceCode)) return;
+
+		Platform.runLater(() -> model.run(currentSourceCode));
+
+		lastSourceCode = currentSourceCode;
 	}
 
 	@Override
